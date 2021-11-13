7d24b07cbc6282ae85c74af0e95158455069967f
RICARDO JUTURI <Ricardojuturi2@gmail.com>
2021-11-12 23:40:30 -0300

    terminado

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.use(express.static('public'));
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname,"views/index.html"))
 })
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname,"views/register.html"))
+})
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname,"views/login.html"))
+})
 
 app.listen(PORT, () => console.log(`El servidor esta escuchando en el
 puerto ${PORT}
